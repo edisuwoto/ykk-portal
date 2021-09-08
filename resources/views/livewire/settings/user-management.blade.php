9922c342f6be97032c70929d6a853bae690ea073
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-08 16:06:43 +0700

    Add permission on create user button

--- resources/views/livewire/settings/user-management.blade.php
+++ resources/views/livewire/settings/user-management.blade.php
@@ -1,13 +1,15 @@
 <div class="m-2 grid grid-cols-12 gap-3">
     <div class="col-span-12">
         <div class="bg-gradient-to-b from-gray-100 to-gray-300 border border-gray-300 shadow">
-            <div class="py-3 px-2">
-                <div class="grid grid-cols-3 lg:grid-cols-6 gap-3">
-                    <x-button>
-                        <span class="text-sm">
-                            <i class="fas fa-plus text-white"></i> {{ __('button.create_new') }}
-                        </span>
-                    </x-button>
+            <div class="py-3 px-2" style="min-height: 3.5rem;">
+                <div class="grid grid-cols-2 lg:grid-cols-6 gap-3">
+                    @if (auth()->user()->hasPermission('user-create'))
+                        <x-button wire:click="new()">
+                            <span class="text-sm">
+                                <i class="fas fa-plus text-white"></i> {{ __('button.create_new') }}
+                            </span>
+                        </x-button>
+                    @endif
                 </div>
             </div>
         </div>
