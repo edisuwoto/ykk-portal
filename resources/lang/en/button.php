57adf842e240e4177c128743ff56133ef29e0a45
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-08 13:56:04 +0700

    Update language

--- resources/lang/en/button.php
+++ resources/lang/en/button.php
@@ -4,4 +4,5 @@ return [
     'log_out'           => 'Log Out',
     'log_in'            => 'Log In',
     'register'          => 'Register',
+    'create_new'        => 'Create New',
 ];
--- resources/lang/id/button.php
+++ resources/lang/id/button.php
@@ -4,4 +4,5 @@ return [
     'log_out'           => 'Keluar',
     'log_in'            => 'Masuk',
     'register'          => 'Daftar',
+    'create_new'        => 'Buat Baru',
 ];
