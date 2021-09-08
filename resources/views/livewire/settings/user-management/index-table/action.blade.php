2b0fa9fef5a3af3f7786ca06b0e762a595e6eda6
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-08 16:06:24 +0700

    Add permission on edit button

--- resources/views/livewire/settings/user-management/index-table/action.blade.php
+++ resources/views/livewire/settings/user-management/index-table/action.blade.php
@@ -1,5 +1,7 @@
 <div class="text-center">
-    <x-button wire:click="edit('{{ \Crypt::encrypt($row->id) }}')">
-        <i class="fas fa-edit"></i>
-    </x-button>
+    @if (auth()->user()->hasPermission('user-edit'))
+        <x-button wire:click="edit('{{ \Crypt::encrypt($row->id) }}')">
+            <i class="fas fa-edit"></i>
+        </x-button>
+    @endif
 </div>
