e29d81aef4a28e08faa942a364393e0289979894
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-08 16:07:24 +0700

    Update badge components

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,3 +1,3 @@
 <span {!! $attributes->merge(['class' => 'inline-flex items-center justify-center px-2 py-1 '.($attributes['size'] ?? 'text-sm').' font-bold leading-none '.($attributes['color'] ?? 'text-gray-100 bg-gray-600').' '.($attributes['rounded'] ?? 'rounded-full')]) !!}>
-    {{ $attributes['title'] ?? $slot }}
+    {{ $attributes['value'] ?? $slot }}
 </span>
