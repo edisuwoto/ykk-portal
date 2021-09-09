55e9c602d16870bc8a6990660cffd1d85e377fc1
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-09 08:22:17 +0700

    Fix auth card

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,11 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
+<div class="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+    @if (isset($logo))
+        <div class="mb-6">
+            {{ $logo }}
+        </div>
+    @endif
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
