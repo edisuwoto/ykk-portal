af598ee1efb10f1821ce1b00e67560a365110d34
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-09 10:12:38 +0700

    Change input type of email

--- resources/views/livewire/settings/user-management/modal/edit-form.blade.php
+++ resources/views/livewire/settings/user-management/modal/edit-form.blade.php
@@ -50,7 +50,7 @@
                 </div>
                 <div>
                     <x-label value="{{__('Email')}}"/>
-                    <x-input type="text" wire:model.defer="form.email" disabled class="text-xs w-full"/>
+                    <x-input type="email" wire:model.defer="form.email" disabled class="text-xs w-full"/>
                     @foreach ($errors->get('form.email') as $message)
                         <div class="text-xs text-red-600">{{ $message }}</div>
                     @endforeach
