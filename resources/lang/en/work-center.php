b6bfb3340114d3a297cfcbeac5e752086d66cca2
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-10 15:00:53 +0700

    Update languages for Work Center

---
+++ resources/lang/en/work-center.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'                 => 'Work Center Master',
+    'navigation'            => 'Master Data > Work Center',
+    'code'                  => 'Work Center Code',
+    'name'                  => 'Work Center Name',
+    'status'                => 'Status',
+    'action'                => 'Actions',
+];
---
+++ resources/lang/id/work-center.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'                 => 'Master Work Center',
+    'navigation'            => 'Master > Work Center',
+    'code'                  => 'Kode Work Center',
+    'name'                  => 'Nama Work Center',
+    'status'                => 'Status',
+    'action'                => 'Aksi',
+];
