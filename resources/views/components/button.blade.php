cc0b94bd99bb166192c384f57e3718a71292069a
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-10 16:17:51 +0700

    Color button can be changed

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,6 @@
-@props(['disabled' => false, 'autofocus' => false])
+@props(['disabled' => false, 'autofocus' => false, 'bgColor' => 'bg-blue-900', 'textColor' => 'text-white'])
 <button
-    {{ $attributes->merge(['type' => 'button', 'class' => 'px-4 py-2 bg-blue-900 items-center border border-transparent focus:border-gray-900 rounded-md font-semibold text-sm text-white text-center focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}
+    {{ $attributes->merge(['type' => 'button', 'class' => 'px-4 py-2 '.$bgColor.' items-center border border-transparent focus:border-gray-900 rounded-md font-semibold text-sm '.$textColor.' text-center focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}
     {{ $disabled ? 'disabled' : '' }}
     {{ $autofocus ? 'autofocus' : '' }}
     style="min-width: 3rem;">
