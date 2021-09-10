1d01d21ee66c3514ee90b4a0f44920846d0556ec
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-10 14:33:24 +0700

    Update languages for Customer

---
+++ resources/lang/en/customer.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'                 => 'Customer Master',
+    'navigation'            => 'Master Data > Customers',
+    'code'                  => 'Customer Code',
+    'name'                  => 'Customer Name',
+    'status'                => 'Status',
+    'action'                => 'Actions',
+];
---
+++ resources/lang/id/customer.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'                 => 'Master Pelanggan',
+    'navigation'            => 'Master > Pelanggan',
+    'code'                  => 'Kode Pelanggan',
+    'name'                  => 'Nama Pelanggan',
+    'status'                => 'Status',
+    'action'                => 'Aksi',
+];
