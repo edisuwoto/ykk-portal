f909668e2a7611c3354ad6135c3fe5dc739566e4
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-10 14:45:13 +0700

    Update language for machines

---
+++ resources/lang/en/machine.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'                 => 'Machine Master',
+    'navigation'            => 'Master Data > Machines',
+    'code'                  => 'Machine Code',
+    'name'                  => 'Machine Name',
+    'status'                => 'Status',
+    'action'                => 'Actions',
+];
---
+++ resources/lang/id/machine.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'                 => 'Master Mesin',
+    'navigation'            => 'Master > Mesin',
+    'code'                  => 'Kode Mesin',
+    'name'                  => 'Nama Mesin',
+    'status'                => 'Status',
+    'action'                => 'Aksi',
+];
