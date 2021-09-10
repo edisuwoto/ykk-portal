a0a5a896910387d6a91e3bb740aba60fe9158451
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-10 14:11:50 +0700

    Update item language for English

---
+++ resources/lang/en/item.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title'                 => 'Item Master',
+    'weight_history_title'  => 'Item Weight History on Production',
+    'navigation'            => 'Master Data > Items',
+    'code'                  => 'Item Code',
+    'name'                  => 'Item Name :number',
+    'active'                => 'Active',
+    'color'                 => 'Color Code',
+    'quantity'              => 'Quantity',
+    'weight'                => 'Weight',
+    'unit'                  => 'Unit',
+    'action'                => 'Action',
+    'picture'               => 'Item Picture',
+    'picture_remove'        => 'Remove Picture',
+    'unit_select'           => 'Select Unit',
+    'wo_number'             => 'WO Number',
+    'production_date'       => 'Production Date',
+];
