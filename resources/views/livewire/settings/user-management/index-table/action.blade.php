0f45a02faf68b495d5c3e48589279ab72c568b7c
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-10 16:18:05 +0700

    Change action button color

--- resources/views/livewire/settings/user-management/index-table/action.blade.php
+++ resources/views/livewire/settings/user-management/index-table/action.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center">
     @if (auth()->user()->hasPermission('user-edit'))
-        <x-button wire:click="edit('{{ \Crypt::encrypt($row->id) }}')">
+        <x-button wire:click="edit('{{ \Crypt::encrypt($row->id) }}')" bgColor="bg-transparent" textColor="text-black">
             <i class="fas fa-edit"></i>
         </x-button>
     @endif
