e21efdb3ab5d733d52f41f8b6d527d0af4cfaa5a
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-10 14:01:45 +0700

    Update language and make for Item

---
+++ resources/lang/id/item.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title'                 => 'Master Item',
+    'weight_history_title'  => 'Histori Berat Item Produksi',
+    'navigation'            => 'Master > Item',
+    'code'                  => 'Kode Item',
+    'name'                  => 'Nama Item :number',
+    'active'                => 'Aktif',
+    'color'                 => 'Kode Warna',
+    'quantity'              => 'Jumlah',
+    'weight'                => 'Berat',
+    'unit'                  => 'Satuan',
+    'action'                => 'Aksi',
+    'picture'               => 'Gambar Item',
+    'picture_remove'        => 'Hapus Gambar',
+    'unit_select'           => 'Pilih Satuan',
+    'wo_number'             => 'Nomor WO',
+    'production_date'       => 'Tanggal Produksi',
+];
