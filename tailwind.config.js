71b7a250488ac47f10afb95985bffa5d19656a3f
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2021-09-28 10:54:59 +0700

    Purge rappasoft views

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     purge: [
+        './vendor/rappasoft/laravel-livewire-tables/resources/views/tailwind/**/*.blade.php',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
